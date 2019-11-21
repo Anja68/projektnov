86d9368c81e80f449e8e588eaccdc1d67ab609db
Anja Leherbauer <anja.leher@gmail.com>
2019-11-21 18:13:36 +0100

    Aenderungen

---
+++ main.py
@@ -0,0 +1,46 @@
+import flask
+import random
+import model
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/fakebook")
+def fakebook():
+    return flask.render_template("Fakebook.html")
+
+
+@app.route("/friseur")
+def friseur():
+    return flask.render_template("friseur.html")
+
+
+@app.route("/about_me")
+def aboutme():
+    return flask.render_template("About_me.html")
+
+@app.route("/base")
+def base():
+    return flask.render_template("base.html")
+
+@app.route("/secret-number-game")
+def secret_number_game():
+    secret = random.randint(1, 10)
+    attemps = 0
+    return flask.render_template("secret_number_game.html", secret_number=secret)
+
+@app.route("/blog")
+def blog():
+    receipe_1 = model.Receipe ("Apfelstrudel", "Cut Apple Bake Sweet", "Sweet")
+    receipe_2 = model.Receipe ("Hamburger", "Fry Meat And Eat", "Sour")
+    receipe_3 = model.Receipe ("Suppe", "Cut Carrots Add Water", "Salty")
+    return flask.render_template("blog.html", receipes=[receipe_1, receipe_2, receipe_3])
+
+
+if __name__ == '__main__':
+    app.run()
